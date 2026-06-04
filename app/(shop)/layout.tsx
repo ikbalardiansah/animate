57cfcbd311bc83c29c750072bf0441aa5ed4c46f
Ikbal Ardiansah <itdevparamitra@gmail.com>
2026-06-04 17:29:46 +0700

    Fixed

--- app/(shop)/layout.tsx
+++ app/(shop)/layout.tsx
@@ -1,13 +1,14 @@
 import Navbar from "@/app/(shop)/shop/components/shop-navbar";
 import Footer from "@/app/(main)/components/footer";
 import WhatsAppFloating from "@/app/(main)/components/whatsapp-floating";
+import type { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Animate By Yunna Mercier",
     description:
     "Local Beauty Brand Skincare lokal viral halal BPOM & terpercaya, hadir untuk kulit sehat dan percaya diri.",
   icons: {
-    icon: "/logo-bulet.jpg",
+    icon: "/logo-square.jpg",
   },
 };
 
